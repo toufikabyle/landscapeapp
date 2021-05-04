699b93bf7182cc4d627079e621bffa0158ff78c8
toufik bouaoun <bouaoun.toufik@gmail.com>
2021-05-04 13:55:20 -0400

    first commit

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Change2 from "./subscreens/changes2";
-import Change from "./subscreens/changes";
-import Change1 from "./subscreens/changes1";
+import Change2 from "./src/changes2";
+import Change from "./src/changes";
+import Change1 from "./src/changes1";
 
 
 
